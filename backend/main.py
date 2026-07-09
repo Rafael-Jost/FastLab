4e828c446ae584e805841dcd0ba4401a6985f25f
Rafael-Jost <rafael2552005@gmail.com>
2026-07-08 21:03:19 -0300

    feat: Initialize FastAPI application with root and basic endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,11 @@
-# FastAPI
+import fastapi
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "FastLab!"}
+
+@app.get("/basic")
+async def basic():
+    return {"message": "OK"}
